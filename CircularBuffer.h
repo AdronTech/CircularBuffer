05b5f7d65c0f866e790f00439da3dbf17a5c66d0
Markus Gögele <markus.goegele@hotmail.com>
2022-12-30 19:25:25 +0100

    Add simple push and pop

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -7,13 +7,28 @@
 
 template <typename Type, int Size>
 class CircularBuffer {
+    Type buffer[Size];
+    uint32_t startIndex;
+    uint32_t endIndex;
+
 public:
-    [[nodiscard]] bool empty() const {
-        return true;
+    void push(Type element) {
+        buffer[startIndex] = element;
+        startIndex++;
+    }
+
+    Type pop() {
+        Type element = buffer[endIndex];
+        endIndex++;
+        return element;
     }
 
     [[nodiscard]] uint32_t count() const {
-        return 0;
+        return startIndex - endIndex;
+    }
+
+    [[nodiscard]] bool empty() const {
+        return count() == 0;
     }
 };
 
--- CircularBufferTest.cpp
+++ CircularBufferTest.cpp
@@ -10,7 +10,34 @@ TEST(CircularBuffer, InitialEmpty) {
     EXPECT_TRUE(buffer.empty()) << "Buffer is not empty initially!";
 }
 
+TEST(CircularBuffer, NotEmptyAfterPush) {
+    auto buffer = CircularBuffer<int, 20>();
+    buffer.push(42);
+    EXPECT_FALSE(buffer.empty()) << "Buffer should not be empty after push";
+}
+
+TEST(CircularBuffer, EmptyAfterPushAndPop) {
+    auto buffer = CircularBuffer<int, 20>();
+    buffer.push(42);
+    auto elem = buffer.pop();
+    EXPECT_EQ(elem, 42) << "Popped element should be equal to pushed element";
+    EXPECT_TRUE(buffer.empty()) << "Buffer should be empty after popping pushed element";
+}
+
 TEST(CircularBuffer, InitialCountZero) {
     auto buffer = CircularBuffer<int, 20>();
     EXPECT_EQ(buffer.count(), 0) << "Buffer count is not zero initially!";
+}
+
+TEST(CircularBuffer, CountCorrectAfterPush) {
+    auto buffer = CircularBuffer<int, 20>();
+    buffer.push(42);
+    EXPECT_EQ(buffer.count(), 1) << "Buffer count does not reflect number of pushes!";
+}
+TEST(CircularBuffer, CountCorrectAfterPushAndPop) {
+    auto buffer = CircularBuffer<int, 20>();
+    buffer.push(42);
+    auto elem = buffer.pop();
+    EXPECT_EQ(elem, 42) << "Popped element should be equal to pushed element";
+    EXPECT_EQ(buffer.count(), 0) << "Buffer count should be zero after popping pushed element";
 }
